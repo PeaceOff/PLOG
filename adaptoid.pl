86090cc5e9ca8f877a2122dda66f2b2006b0bb19
PeaceOff <davidazevedo1996@hotmail.com>
2016-10-18 21:40:31 +0100

    Merged the changes implemented in class

--- adaptoid.pl
+++ adaptoid.pl
@@ -1,53 +1,60 @@
 tabuleiro( [
-            [a,'','','',''],
-      		[b,'','','','',''],
-      		[c,'','','','','',''],
-      		[#,'',[0,'W',0,0],'','','',[0,'B',0,0],''],
-			[c,#,'','','','','',''],
-      		[b,#,#,'','','','',''],
-      		[a,#,#,#,'','','','']
+            [a,vazio,vazio,vazio,vazio],
+      		[b,vazio,vazio,vazio,vazio,vazio],
+      		[c,vazio,vazio,vazio,vazio,vazio,vazio],
+      		[#,vazio,[branco,6,6],vazio,vazio,vazio,[preto,5,5],vazio],
+			[c,#,vazio,vazio,vazio,vazio,vazio,vazio],
+      		[b,#,#,vazio,vazio,vazio,vazio,vazio],
+      		[a,#,#,#,vazio,vazio,vazio,vazio]
            ]
           ).
 
-desenharMember(0,S):- write('').
+/*TO DO
+    Diminiuir o lixo que e impresso no ecra;
+*/
+
+desenharCor(branco) :- write('X').
+desenharCor(preto) :- write('O').
+
+desenharMember(0,_):- write('').
 desenharMember(N,S):- N > 0, N < 6, write(S), N1 is N - 1, desenharMember(N1,S).
 desenharMember(6,S):- write(S),write(S),write(S),write(S),write(S).
 
 desenharMemberC(6,S):- write(S).
-desenharMemberC(N,S):- N < 6, write(' ').
+desenharMemberC(N,_):- N < 6, write(' ').
 
 desenharEspaco(0):- write('').
 desenharEspaco(N):- N > 0, N < 6, write(' '), N1 is N - 1, desenharEspaco(N1).
-desenharEspaco(N):- write('').
+desenharEspaco(_):- write('').
 desenharResto(N):-  R is 5-N, desenharEspaco(R).
 
-desenharC(a):-      write('           ').
-desenharC(b):-      write('       ').
-desenharC(c):-      write('    ').
-desenharC(#):-      write('').
-desenharC(''):-     write('/     \\').
-desenharC([Id,A,B,C]):-validaPeca(B,C), write('/'), desenharMember(B,'Y'), desenharResto(B), write('\\').
+desenharC(a):-       write('           ').
+desenharC(b):-       write('       ').
+desenharC(c):-       write('    ').
+desenharC(#):-       write('').
+desenharC(vazio):-   write('/     \\').
+desenharC([_,B,_]):- write('/'), desenharMember(B,'Y'), desenharResto(B), write('\\').
 
 desenharM(a):-      desenharC(a).
 desenharM(b):-      desenharC(b).
 desenharM(c):-      desenharC(c).
 desenharM(#):-      desenharC(#).
-desenharM(''):-     write('|     |').
-desenharM([Id,A,B,C]):-write('|'), desenharMemberC(B,'Y') ,write(Id), write(A) , write(Id) ,  desenharMemberC(C,'L'), write('|').
+desenharM(vazio):-     write('|     |').
+desenharM([A,B,C]):-write('|'), desenharMemberC(B,'Y') ,write(' '), desenharCor(A) , write(' '),  desenharMemberC(C,'L'), write('|').
 
 desenharB(a):-      desenharC(a).
 desenharB(b):-      desenharC(b).
 desenharB(c):-      desenharC(c).
 desenharB(#):-      desenharC(#).
-desenharB(''):-     write('\\     /').
-desenharB([Id,A,B,C]):-write('\\'), desenharMember(C,'L'), desenharResto(C), write('/').
+desenharB(vazio):-     write('\\     /').
+desenharB([_,_,C]):-write('\\'), desenharMember(C,'L'), desenharResto(C), write('/').
 
 desenharS(a):-      desenharC(a).
 desenharS(b):-      desenharC(b).
 desenharS(c):-      desenharC(c).
 desenharS(#):-      desenharC(#).
-desenharS(''):-     write(' ¯¯¯¯¯ ').
-desenharS([Id,A,B,C]):-write(' ¯¯¯¯¯ ').
+desenharS(vazio):-     write(' Â¯Â¯Â¯Â¯Â¯ ').
+desenharS([_,_,_]):-write(' Â¯Â¯Â¯Â¯Â¯ ').
 
 desenharLinhaC([X|Xs]):- desenharC(X), desenharLinhaC(Xs).
 desenharLinhaC([]):- nl.
@@ -64,7 +71,9 @@ desenharLinha([]):- write('').
 desenharTabuleiro( [ X | Xs ]) :- desenharLinha(X), desenharTabuleiro(Xs).
 desenharTabuleiro([]) :- nl.
 
-validaPeca([Id,A,B,C]):- A is B + C, A < 7.
+getPecaXY(t,)
+
+validaPeca([A,B,C]):- A is B + C, A < 7.
 validaPeca(B,C):- A is B + C,A < 7.
 
 /*
