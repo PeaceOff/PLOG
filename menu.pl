cdc04a5623036cee80e18bb3f9f826890724828e
David <davidazevedo1996@hotmail.com>
2016-11-04 14:50:38 +0000

    Added new menus

--- menu.pl
+++ menu.pl
@@ -1,4 +1,3 @@
-
 clearScreen :- clearConsole(50).
 
 clearConsole(0).
@@ -15,7 +14,9 @@ welcome :-  clearScreen,
             write('|                          | |                           |'), nl,
             write('|                          |_|                           |'), nl,
             write('|                                                        |'), nl,
+            write('|                                                        |'), nl,
             write('|                        WELCOME!                        |'), nl,
+            write('|                                                        |'), nl,
             write('|                                                        |'), nl,
             write('| Made By:                                               |'), nl,
             write('|     David Azevedo up201405846                          |'), nl,
@@ -27,24 +28,61 @@ welcome :-  clearScreen,
 
 startMenu :-    clearScreen,
                 write(' -------------------------------------------------------- '), nl,
+                write('|                   _             _        _     _       |'), nl,
+                write('|          /\\      | |           | |      (_)   | |      |'), nl,
+                write('|         /  \\   __| | __ _ _ __ | |_ ___  _  __| |      |'), nl,
+                write('|        / /\\ \\ / _` |/ _` | \'_ \\| __/ _ \\| |/ _` |      |'), nl,
+                write('|       / ____ \\ (_| | (_| | |_) | || (_) | | (_| |      |'), nl,
+                write('|      /_/    \\_\\__,_|\\__,_| .__/ \\__\\___/|_|\\__,_|      |'), nl,
+                write('|                          | |                           |'), nl,
+                write('|                          |_|                           |'), nl,
                 write('|                                                        |'), nl,
                 write('|                                                        |'), nl,
+                write('|   Escolha uma das seguintes opcoes :                   |'), nl,
                 write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
-                write('|                                                        |'), nl,
+                write('|     1. Jogar                                           |'), nl,
+                write('|     2. Tutorial                                        |'), nl,
+                write('|     3. Sobre                                           |'), nl,
+                write('|     4. Sair                                            |'), nl,
                 write('|                                                        |'), nl,
                 write('|                Press enter to continue!                |'), nl,
                 write(' -------------------------------------------------------- '), nl,
-                get_char(_).
+                get_char(A), get_char(_),%How to tirar apenas um character de input
+                (A = '1' -> jogarMenu;
+                A = '2' -> startMenu;
+                A = '3' -> startMenu;
+                A = '4' -> sair;
+                startMenu).
+
+sair.
+
+jogarMenu :-    clearScreen,
+                write(' -------------------------------------------------------- '), nl,
+                write('|                   _             _        _     _       |'), nl,
+                write('|          /\\      | |           | |      (_)   | |      |'), nl,
+                write('|         /  \\   __| | __ _ _ __ | |_ ___  _  __| |      |'), nl,
+                write('|        / /\\ \\ / _` |/ _` | \'_ \\| __/ _ \\| |/ _` |      |'), nl,
+                write('|       / ____ \\ (_| | (_| | |_) | || (_) | | (_| |      |'), nl,
+                write('|      /_/    \\_\\__,_|\\__,_| .__/ \\__\\___/|_|\\__,_|      |'), nl,
+                write('|                          | |                           |'), nl,
+                write('|                          |_|                           |'), nl,
+                write('|                                                        |'), nl,
+                write('|                                                        |'), nl,
+                write('|   Escolha uma das seguintes opcoes :                   |'), nl,
+                write('|                                                        |'), nl,
+                write('|     1. Humano vs Humano                                |'), nl,
+                write('|     2. Humano vs Computador                            |'), nl,
+                write('|     3. Computador vs Computador                        |'), nl,
+                write('|     4. Sair                                            |'), nl,
+                write('|                                                        |'), nl,
+                write('|                Press enter to continue!                |'), nl,
+                write(' -------------------------------------------------------- '), nl,
+                get_char(A), get_char(_),%How to tirar apenas um character de input
+                (A = '1' -> jogar(hh);
+                A = '2' -> startMenu;
+                A = '3' -> startMenu;
+                A = '4' -> sair;
+                startMenu).
 
 testSwitch :-   read(A), (%(CONDITION -> if true ; if false)
                 (A == 1, 5 < 4) -> write('Escolheu 1');
