b3b6fbb61f5b9615f9c904836b05f6bb10bf8d14
David Joel Nogueira Azevedo <davidazevedo1996@hotmail.com>
2016-12-13 18:08:02 +0000

    Fixed buildTPC predicate

--- TP2/solucao.pl
+++ TP2/solucao.pl
@@ -9,12 +9,12 @@ horarios(   [[0,1,2],[4,1,2],[3,2,0],[5,4,3],[0,1,2]],
             [[5,4,3],[0,1,2],[0,1,2],[4,1,2],[3,2,0]],
             [[5,4,3],[0,1,2],[0,1,2],[4,1,2],[3,2,0]]).
 
-solucao(NumeroSemanas,Horarios,Disciplinas,NmaxTPCDia,NmaxTPCDisciplina).
+%solucao(NumeroSemanas,Horarios,Disciplinas,NmaxTPCDia,NmaxTPCDisciplina).
 
 buildTPCAux([],_,[]).
-buildTPCAux([Disc,Ds],NSemanas,[Tpc|Ts]) :- length(B,NSemanas), Tpc = [Disc,B], buildTPCAux(Ds,NSemanas,Ts).
+buildTPCAux([Disc|Ds],NSemanas,[Tpc|Ts]) :- length(B,NSemanas), Tpc = [Disc,B], buildTPCAux(Ds,NSemanas,Ts).
 buildTPCDay([],_,[]).
-buildTPCDay([Dia|Ds],NSemanas,[Tpc|Ts]) :- l(ength(Dia,NDisc), length(Tpc,NDisc), buildTPCAux(Dia,NSemanas,Tpc), buildTPCDay(Ds,NSemanas,Ts).
+buildTPCDay([Dia|Ds],NSemanas,[Tpc|Ts]) :- length(Dia,NDisc), length(Tpc,NDisc), buildTPCAux(Dia,NSemanas,Tpc), buildTPCDay(Ds,NSemanas,Ts).
 buildTPCWeek([],_,[]).
 buildTPCWeek([Horario|Hs],NSemanas,[R|Rs]) :- length(R,5), buildTPCDay(Horario,NSemanas,R), buildTPCWeek(Hs,NSemanas,Rs).
 buildTPC(NTurmas,NSemanas,Horarios,Res) :- length(Res,NTurmas), buildTPCWeek(Horarios,NSemanas,Res).
