bfb8c81d23d67314573ce8366d8ed3e325cbb28f
David Joel Nogueira Azevedo <davidazevedo1996@hotmail.com>
2016-12-18 23:09:58 +0000

    Resolução ainda muito incompleta, dei o meu melhor, haja saude

--- TP2/horarios.pl
+++ TP2/horarios.pl
@@ -11,6 +11,8 @@ disciplina(2,'Portugues ').
 disciplina(3,'Historia  ').
 disciplina(4,'Ciencias  ').
 disciplina(5,'Artes     ').
+disciplina(6,'EVT       ').
+disciplina(7,'Ingles    ').
 
 semana(1,'Segunda').
 semana(2,'Terca').
@@ -30,23 +32,27 @@ nome_discip(2,'Portugues ').
 nome_discip(3,'Historia  ').
 nome_discip(4,'Ciencias  ').
 nome_discip(5,'Artes     ').
+nome_discip(6,'EVT       ').
+nome_discip(7,'Ingles    ').
 
 tpc2_disciplinas([1,2,3,4,5]).
 tpc2_disciplinas2([1,2,3,4,5]).
+tpc2_disciplinas3([1,2,3,4,5,6]).
+tpc2_disciplinas4([1,2,3,4,5,6,7]).
 tpc2_semana1([ [1,2,3] , [4,3] , [3,5] , [3,2] , [3,2,5] ]).
 tpc2_semana2([ [2,1], [1] , [1,3] , [4] , [5] ]).
 
 teste_turma1([[1,2,3],[4,2,3],[5,2,1],[3,4,2],[2,3,1]]).
 teste_turma2([[3,2,1],[2,4,1],[2,2,1],[1,2,3,4,5],[2,3,1]]).
 
-teste_turmas1([%4 turmas e 5 disciplinas
+teste_turmas1([%4 turmas e 5 disciplinas Funciona nos tpcs tambem
               [[1,2,3],[4,2,3],[5,2,1],[3,4,2],[2,3,1]],
               [[3,2,1],[2,4,1],[2,5,1],[3,4,5],[2,3,1]],
               [[1,2,3],[4,2,3],[5,2,1],[2,3,1],[3,4,2]],
               [[5,2,1],[4,2,3],[1,2,3],[3,4,2],[2,3,1]]
             ]).
 
-teste_turmas2([%13 turmas e 5 disciplinas
+teste_turmas2([%13 turmas e 5 disciplinas - Funciona nos tpcs tambem
               [[1,2,3],[4,2,3],[5,2,1],[3,4,2],[2,3,1]],
               [[3,2,1],[2,4,1],[2,5,1],[3,4,5],[2,3,1]],
               [[1,2,3],[4,2,3],[5,2,1],[2,3,1],[3,4,2]],
@@ -91,4 +97,31 @@ trabalhoPratico2(NSemanas,NumeroTPCs) :-
     nl , nl , write('|           TPCS           |'), nl, nl,
     get_char(_),
     tpc2_disciplinas(Disciplinas),
+    calculaTpcs(Horarios,1,Disciplinas, NSemanas, NumeroTPCs,2).
+
+trabalhoPratico22(NSemanas,NumeroTPCs) :-
+    teste_turmas2(Horarios),
+    write('|          TESTES          |'), nl, nl,
+    resolveTestesPorFavor(Horarios,5,NSemanas),%Numero de Disciplinas
+    nl , nl , write('|           TPCS           |'), nl, nl,
+    get_char(_),
+    tpc2_disciplinas(Disciplinas),
+    calculaTpcs(Horarios,1,Disciplinas, NSemanas, NumeroTPCs,2).
+
+trabalhoPratico23(NSemanas,NumeroTPCs) :-%TPCS 4.
+    teste_turmas3(Horarios),
+    write('|          TESTES          |'), nl, nl,
+    resolveTestesPorFavor(Horarios,6,NSemanas),%Numero de Disciplinas
+    nl , nl , write('|           TPCS           |'), nl, nl,
+    get_char(_),
+    tpc2_disciplinas3(Disciplinas),
+    calculaTpcs(Horarios,1,Disciplinas, NSemanas, NumeroTPCs,2).
+
+trabalhoPratico24(NSemanas,NumeroTPCs) :-%TPCS 4.
+    teste_turmas4(Horarios),
+    write('|          TESTES          |'), nl, nl,
+    resolveTestesPorFavor(Horarios,7,NSemanas),%Numero de Disciplinas
+    nl , nl , write('|           TPCS           |'), nl, nl,
+    get_char(_),
+    tpc2_disciplinas4(Disciplinas),
     calculaTpcs(Horarios,1,Disciplinas, NSemanas, NumeroTPCs,2).
